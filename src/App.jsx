cf3254878c3404f5e5d4e3b69c35db8603e9c838
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-01-28 17:34:33 +0700

    Remove: Unused page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Home from './pages';
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Service from './components/Services'
